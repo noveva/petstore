b78771f4dd4851bdd812886c58d393f468a660fa
noveva <nat.solovieva@gmail.com>
2024-01-13 15:02:51 +0100

    Added loading of pets from API and loading / nothing found / error state to pets list

--- src/app/pets-list/pets-list.component.ts
+++ src/app/pets-list/pets-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { MOCK_PETS } from './mock-pets';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PetCardComponent } from './card/card.component';
 import { Pet } from './pets.typings';
+import { PetstoreService } from '../services/petstore/petstore.service';
 
 @Component({
   selector: 'app-pets-list',
@@ -10,6 +11,29 @@ import { Pet } from './pets.typings';
   templateUrl: './pets-list.component.html',
   styleUrl: './pets-list.component.scss'
 })
-export class PetsListComponent {
-  pets: Pet[] = MOCK_PETS;
+export class PetsListComponent implements OnInit {
+  pets?: Pet[];
+
+  constructor(
+    private petStoreService: PetstoreService,
+    private destroyRef: DestroyRef
+  ) {}
+
+  ngOnInit(): void {
+    this.getPets();
+  }
+
+  private getPets() {
+    this.petStoreService
+      .getPets()
+      .pipe(takeUntilDestroyed(this.destroyRef)) // operator in dev preview, too tempting to not to give it a go in a pet project
+      .subscribe({
+        next: pets => {
+          this.pets = pets;
+        },
+        error: () => {
+          this.pets = [];
+        }
+      });
+  }
 }
---
+++ src/app/services/petstore/petstore.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { PET_STATUS, Pet } from '../../pets-list/pets.typings';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetstoreService {
+  constructor(private http: HttpClient) {}
+
+  getPets(status: PET_STATUS = 'available'): Observable<Pet[]> {
+    return this.http
+      .get<Pet[]>(
+        `https://petstore3.swagger.io/api/v3/pet/findByStatus?status=${status}`
+      )
+      .pipe(
+        // TODO handle errors in catchError
+        map(pets => {
+          return pets.map(
+            ({ name, status, photoUrls }) =>
+              <Pet>{
+                name,
+                status,
+                photoUrls
+              }
+          );
+        })
+      );
+  }
+}
